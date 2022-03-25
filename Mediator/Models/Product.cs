a35217d47b0901715543ef91f8ee50663801f94c
diana.iminza <diana.iminza@ronforddigital.com>
2022-03-25 10:49:29 +0300

    my first commit

---
+++ Mediator/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Mediator.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string Description { get; set; }
+        public decimal Rate { get; set; }
+        public decimal BuyingPrice { get; set; }
+        public string ConfidentialData { get; set; }
+
+        public string SellingPrice { get; set; }
+
+        public double Profit { get; set; }
+
+        public string Review { get; set; }
+    }
+}
